2678e6823352fbe53163a92350fad95a8306b63f
Jenei Péter <jeneipeter001@gmail.com>
2022-01-09 13:42:53 +0100

    Start gomb, táblázat kész, listák kicserélve egy osztállyal (yay OOP)

---
+++ Csigaverseny/CsigaVersenyzo.cs
@@ -0,0 +1,57 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Controls;
+using System.Windows.Shapes;
+
+namespace Csigaverseny
+{
+    class CsigaVersenyzo
+    {
+        private Rectangle csigaObj;
+        private string nev;
+        private int osszPont = 0;
+        private bool celbaErt = false;
+        private Rectangle palya;
+        private List<int> helyezesek = new List<int>();
+        private Label versenyHelyezes;
+
+        public CsigaVersenyzo(Rectangle csiga, Rectangle palya, Label versenyHelyezes, string nev)
+        {
+            this.csigaObj = csiga;
+            this.nev = nev;
+            this.palya = palya;
+            this.versenyHelyezes = versenyHelyezes;
+
+            this.helyezesek.Add(0); // #1
+            this.helyezesek.Add(0); // #2
+            this.helyezesek.Add(0); // #3
+        }
+
+        public int OsszesPont { get { return osszPont; } set { osszPont = value; } }
+        public string Nev { get { return nev; } }
+        public bool CelbaErt { get { return celbaErt; } set { celbaErt = value; } }
+        public List<int> Helyezesek { get { return helyezesek; } set { helyezesek = value; } }
+        public Rectangle CsigaObject { get { return csigaObj; } }
+        public Rectangle Palya { get { return palya; } set { palya = value; } }
+        public Label VersenyHelyezes { get { return versenyHelyezes; } }
+
+        public static List<CsigaVersenyzo> BajnoksagAllas(List<CsigaVersenyzo> csigak)
+        {
+            var pontok = new List<int>();
+            pontok.Add(csigak[0].OsszesPont);
+            pontok.Add(csigak[1].OsszesPont);
+            pontok.Add(csigak[2].OsszesPont);
+
+            var allas = new List<CsigaVersenyzo>();
+
+            allas.Add(csigak[pontok.IndexOf(pontok.Max())]); // #1
+            allas.Add(csigak[pontok.IndexOf(pontok.Min())]); // #3
+            allas.Insert(1, csigak.Find(x => !allas.Contains(x))); //#2
+
+            return allas;
+        }
+    }
+}
--- Csigaverseny/MainWindow.xaml.cs
+++ Csigaverseny/MainWindow.xaml.cs
@@ -16,18 +16,13 @@ using System.Windows.Threading;
 
 namespace Csigaverseny
 {
-    /// <summary>
-    /// Interaction logic for MainWindow.xaml
-    /// </summary>
     public partial class MainWindow : Window
     {
         DispatcherTimer idozito = new DispatcherTimer();
-        List<int> osszPontok = new List<int>();
-        List<bool> celbaErt = new List<bool>();
         List<Rectangle> csigak = new List<Rectangle>();
-        List<Rectangle> palyak = new List<Rectangle>();
         List<Rectangle> helyek = new List<Rectangle>();
-        List<Label> helyezesek = new List<Label>();
+
+        List<CsigaVersenyzo> versenyzok = new List<CsigaVersenyzo>();
 
         public MainWindow()
         {
@@ -48,91 +43,75 @@ namespace Csigaverseny
                 sebessegek.Add(rnd.Next(1, 5));
             }
 
-            //helyezesek[sebessegek.IndexOf(sebessegek.Max())] = 1;
-            //helyezesek[sebessegek.IndexOf(sebessegek.Min())] = 3;
-            //int second = helyezesek.FindIndex(x => x != sebessegek.Max() && x != sebessegek.Min());
-            //helyezesek[second] = 2;
-
-            //this.osszPontok[sebessegek.IndexOf(sebessegek.Max())] = 3;
-            //this.osszPontok[sebessegek.IndexOf(sebessegek.Min())] = 1;
-            //int second = osszPontok.FindIndex(x => x != sebessegek.Max() && x != sebessegek.Min());
-            //this.osszPontok[second] = 2;
-
-
             // Csigák mozgatása a sebességek lista alapján
-            for (int i = 0; i < csigak.Count; i++)
+            for (int i = 0; i < versenyzok.Count; i++)
             {
                 // Ha még nem ért célba...
-                if (!celbaErt[i])
+                if (!versenyzok[i].CelbaErt)
                 {
                     // ...mozgassa jobbra az adott random értékkel
-                    double leftMargin = csigak[i].Margin.Left + Convert.ToDouble(sebessegek[i]);
-                    double topMargin = csigak[i].Margin.Top;
-                    csigak[i].Margin = new Thickness(leftMargin, topMargin, 0, 0);
+                    double leftMargin = versenyzok[i].CsigaObject.Margin.Left + Convert.ToDouble(sebessegek[i]);
+                    double topMargin = versenyzok[i].CsigaObject.Margin.Top;
+                    versenyzok[i].CsigaObject.Margin = new Thickness(leftMargin, topMargin, 0, 0);
 
                     // Ha elérte a célt
-                    if (csigak[i].Margin.Left >= 800)
+                    if (versenyzok[i].CsigaObject.Margin.Left >= 800)
                     {
+                        // #1
                         if (helyek.Count == 0)
                         {
                             helyek.Add(csigak[i]);
-                            palyak[i].Fill = Brushes.Gold;
-                            osszPontok[i] += 3;
-                            helyezesek[i].Content = "1";
-                            celbaErt[i] = true;
+                            versenyzok[i].Palya.Fill = Brushes.Gold;
+                            versenyzok[i].OsszesPont += 3;
+                            versenyzok[i].Helyezesek[0]++;
+                            //helyezesek[i].Content = "1";
+                            versenyzok[i].VersenyHelyezes.Content = "1";
+                            versenyzok[i].CelbaErt = true;
                         }
+                        // #2
                         else if(helyek.Count == 1)
                         {
                             helyek.Add(csigak[i]);
-                            palyak[i].Fill = Brushes.Silver;
-                            osszPontok[i] += 2;
-                            helyezesek[i].Content = "2";
-                            celbaErt[i] = true;
+                            versenyzok[i].Palya.Fill = Brushes.Silver;
+                            versenyzok[i].OsszesPont += 2;
+                            versenyzok[i].Helyezesek[1]++;
+                            //helyezesek[i].Content = "2";
+                            versenyzok[i].VersenyHelyezes.Content = "2";
+                            versenyzok[i].CelbaErt = true;
                         }
+                        // #3
                         else if (helyek.Count == 2)
                         {
                             helyek.Add(csigak[i]);
-                            palyak[i].Fill = Brushes.SandyBrown;
-                            celbaErt[i] = true;
-                            osszPontok[i] += 1;
-                            helyezesek[i].Content = "3";
+                            versenyzok[i].Palya.Fill = Brushes.SandyBrown;
+                            versenyzok[i].CelbaErt = true;
+                            versenyzok[i].OsszesPont += 1;
+                            versenyzok[i].Helyezesek[2]++;
+                            //helyezesek[i].Content = "3";
+                            versenyzok[i].VersenyHelyezes.Content = "3";
                             idozito.Stop();
 
                             ujBajnoksagBtn.IsEnabled = true;
                             ujFutamBtn.IsEnabled = true;
+
+                            MutasdBajnoksagAllast();
                         }
                     }
 
                 }
             }
-
         }
 
         private void KezdoInicializalas()
         {
-            // Összes pontok
-            osszPontok.Add(0); // csiga1
-            osszPontok.Add(0); // csiga2
-            osszPontok.Add(0); // csiga3
-
-            helyezesek.Add(helyezes1);
-            helyezesek.Add(helyezes2);
-            helyezesek.Add(helyezes3);
-
-            // Célba értek e
-            celbaErt.Add(false);
-            celbaErt.Add(false);
-            celbaErt.Add(false);
+            // Csiga versenyzők
+            versenyzok.Add(new CsigaVersenyzo(csiga1, palya1, helyezes1, "csiga1"));
+            versenyzok.Add(new CsigaVersenyzo(csiga2, palya2, helyezes2, "csiga2"));
 
             // Csigák lista
             csigak.Add(csiga1);
             csigak.Add(csiga2);
             csigak.Add(csiga3);
-
-            // Pályák listája
-            palyak.Add(palya1);
-            palyak.Add(palya2);
-            palyak.Add(palya3);
         }
 
         private void startBtn_Click(object sender, RoutedEventArgs e)
@@ -141,6 +120,36 @@ namespace Csigaverseny
             startBtn.IsEnabled = false;
             ujBajnoksagBtn.IsEnabled = false;
             ujFutamBtn.IsEnabled = false;
+        }
+
+        private void MutasdBajnoksagAllast()
+        {
+            var allas = CsigaVersenyzo.BajnoksagAllas(versenyzok);
+
+            // Név
+            allasNev_1.Content = allas[0].Nev;
+            allasNev_2.Content = allas[1].Nev;
+            allasNev_3.Content = allas[2].Nev;
+
+            // Nyertes érmei
+            allas1_1.Content = allas[0].Helyezesek[0];
+            allas2_1.Content = allas[0].Helyezesek[1];
+            allas3_1.Content = allas[0].Helyezesek[2];
+
+            // 2. hely érmei
+            allas1_2.Content = allas[1].Helyezesek[0];
+            allas2_2.Content = allas[1].Helyezesek[1];
+            allas3_2.Content = allas[1].Helyezesek[2];
+
+            // 3. hely érmei
+            allas3_1.Content = allas[2].Helyezesek[0];
+            allas3_2.Content = allas[2].Helyezesek[1];
+            allas3_3.Content = allas[2].Helyezesek[2];
+
+            // Pont
+            allasPont_1.Content = $"{allas[0].OsszesPont} p";
+            allasPont_2.Content = $"{allas[1].OsszesPont} p";
+            allasPont_3.Content = $"{allas[2].OsszesPont} p";
         }
     }
 }
